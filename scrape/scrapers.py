6acc40cc34f2a4e6371ed693649381257b9e8a3d
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-09-11 22:28:39 -0700

    Add auto mode with no user prompts

--- scrape/scrapers.py
+++ scrape/scrapers.py
@@ -23,35 +23,52 @@ class HTMLScraper(object):
                 f.write(r.content)
 
 class SimpleHTMLScraper(HTMLScraper):
-    def __init__(self, url, outputdir):
+    def __init__(self, url, outputdir, xpath_inp=None, ext_inp=None):
         super(SimpleHTMLScraper, self).__init__(url, outputdir)
+        oneshot = (xpath_inp is not None) and (ext_inp is not None)
         while True:
-            xpath = input('Enter Full XPath to encompassing element (ENTER to exit): ')
+            if xpath_inp is None:
+                xpath = input('Enter Full XPath to encompassing element (ENTER to exit): ')
+            else:
+                xpath = xpath_inp
             if not xpath:
                 break
-            text_spec = input('...Special text that filenames should contain (ENTER for none): ')
+            if ext_inp is None:
+                text_spec = input('...Special text that filenames should contain (ENTER for none): ')
+            else:
+                text_spec = ext_inp
             xpath_results = self.tree.xpath(xpath)
             if len(xpath_results) > 0:
                 try:
                     for xpath_child in self.get_xpath_children(self.tree.xpath(xpath)[0]):
                         self.download_from_url_xpath(xpath_child, text_spec)
                 except TypeError:
-                    print('XPath element does not appear to have the child elements you\'re searching for; please try again...')
+                    if oneshot:
+                        print('XPath element does not appear to have the child elements you\'re searching for')
+                        break
+                    else:
+                        print('XPath element does not appear to have the child elements you\'re searching for; please try again...')
             else:
-                print('XPath turned no results; please try again...')
-                yn = input('Would you like me to print out the entire XPath tree [y/N]: ')
-                if yn.lower() == 'y':
-                    print()
-                    for e in self.tree.getroot().iter():
-                        print(self.tree.getpath(e))
-                    print()
+                if oneshot:
+                    print('XPath turned no results')
+                    break
+                else:
+                    print('XPath turned no results; please try again...')
+                    yn = input('Would you like me to print out the entire XPath tree [y/N]: ')
+                    if yn.lower() == 'y':
+                        print()
+                        for e in self.tree.getroot().iter():
+                            print(self.tree.getpath(e))
+                        print()
+            if oneshot:
+                break
 
     def get_xpath_children(self, xpath):
         return []
 
 class SimpleLinkScraper(SimpleHTMLScraper):
     def __init__(self, args):
-        super(SimpleLinkScraper, self).__init__(args.page, args.output)
+        super(SimpleLinkScraper, self).__init__(args.page, args.output, args.xpath, args.ext)
     
     def get_xpath_children(self, root):
         xpath_children = list()
@@ -62,7 +79,7 @@ class SimpleLinkScraper(SimpleHTMLScraper):
 
 class SimpleImageScraper(SimpleHTMLScraper):
     def __init__(self, args):
-        super(SimpleImageScraper, self).__init__(args.page, args.output)
+        super(SimpleImageScraper, self).__init__(args.page, args.output, args.xpath, args.ext)
     
     def get_xpath_children(self, root):
         xpath_children = list()
